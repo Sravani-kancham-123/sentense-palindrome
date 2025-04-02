096925a198f9a3a6d48bbbbb63e2be77f319a333
Sravani-kancham-123 <kanchamsravani3@gmail.com>
2025-04-02 21:48:57 +0530

    Add files via upload
    here , we are reversing the whole words in a sentense.

---
+++ sentense-palindrome.js
@@ -0,0 +1,8 @@
+var str = "sravani is a good girl";
+var newString = "";
+for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i]; 
+}
+
+console.log("The reversed string is:", newString);
+
